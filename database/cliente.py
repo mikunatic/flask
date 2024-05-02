3a9649f574a58e9a33a1cd1ff356120515c92bd4
Thiago <martins.fm28@outlook.com>
2024-05-02 08:30:42 -0300

    Botão de adicionar cliente funcionando, já atualizando a página para mostrar o cliente que foi cadastrado em tempo de requisição

---
+++ database/cliente.py
@@ -0,0 +1,8 @@
+CLIENTES = [
+    {"id":1, "nome": "Mila", "email": "thiago@mail.com"},
+    {"id":2, "nome": "Baby", "email": "baby@mail.com"},
+    {"id":3, "nome": "Gorila", "email": "gorila@mail.com"},
+    {"id":4, "nome": "Capi", "email": "capi@mail.com"},
+    {"id":5, "nome": "Cabecinha", "email": "cabecinha@mail.com"},
+    {"id":6, "nome": "Vininoide", "email": "vininoide@mail.com"},
+]
--- routes/cliente.py
+++ routes/cliente.py
@@ -1,4 +1,5 @@
-from flask import Blueprint
+from flask import Blueprint, render_template, request
+from database.cliente import CLIENTES
 
 cliente_route = Blueprint('cliente', __name__)
 
@@ -6,31 +7,38 @@ cliente_route = Blueprint('cliente', __name__)
 @cliente_route.route('/')  # /clientes/
 def lista_clientes():
     """Listar os clientes"""
-    return render_template('lista_clientes.html')
+    return render_template('lista_clientes.html', clientes=CLIENTES)
 
 
 @cliente_route.route('/', methods=['POST'])
 def inserir_cliente():
     """Inserir os dados do cliente"""
-    pass
+    data = request.json
+    novo_usuario = {
+        "id":len(CLIENTES)+1,
+        "nome": data['nome'],
+        "email":data['email'],
+    }
+    CLIENTES.append(novo_usuario)
+    return render_template('item_cliente.html', cliente=novo_usuario)
 
 
 @cliente_route.route('/new')
 def form_cliente():
     """ Formulário para cadastrar um cliente """
-    return {'pagina': "formulario clientes"}
+    return render_template('form_cliente.html')
 
 
 @cliente_route.route('/<int:cliente_id>')
 def detalhe_cliente(cliente_id):
     """ Exibir detalhes de um cliente """
-    pass
+    return render_template('detalhe_cliente.html')
 
 
 @cliente_route.route('/<int:cliente_id>/edit')
 def form_edit_cliente(cliente_id):
     """ Formulario para editar um cliente """
-    pass
+    return render_template('form_edit_cliente.html')
 
 
 @cliente_route.route('/<int:cliente_id>/update', methods=['PUT'])
